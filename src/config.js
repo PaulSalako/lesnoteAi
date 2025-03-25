67fbf9e856ffe7f808013bca3eeea6cbc3937788
Paul Salako <Salakopaul1@gmail.com>
2025-03-25 11:00:20 +0100

    comment

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-// src/config.js
-// const API_BASE_URL = 'https://localhost:7225';
+// src/config.jsss
+// const API_BASE_URL = 'https://localhost:7225';`
 const API_BASE_URL = 'https://lesnote-api-prod-d9f7bmh4f9ejbbe9.westeurope-01.azurewebsites.net';
 export const API_URL = `${API_BASE_URL}/api`;
 export default API_BASE_URL;
